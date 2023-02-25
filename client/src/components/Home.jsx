93725ac770296635b6e06bd9fd850483cc9eda0a
Brandon Alvarez <balvarez325@gmail.com>
2023-02-25 12:31:19 -0500

    set home page template layout + css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-screen">
+      <div className="top-div">Top</div>
+      <div className="photo-grid">
+        <div className="photo-grid-item">Photo 1</div>
+        <div className="photo-grid-item">Photo 2</div>
+        <div className="photo-grid-item">Photo 3</div>
+        <div className="photo-grid-item">Photo 4</div>
+        <div className="photo-grid-item">Photo 5</div>
+        <div className="photo-grid-item">Photo 6</div>
+        <div className="photo-grid-item">Photo 7</div>
+        <div className="photo-grid-item">Photo 8</div>
+        <div className="photo-grid-item">Photo 9</div>
+      </div>
+      <div className="upload-div">Upload</div>
+    </div>
+  );
 }
 
 export default Home;
