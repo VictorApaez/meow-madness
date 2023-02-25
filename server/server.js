93d3c11dc81def8978469916e2e157baba843246
VictorApaez <vic.apaez@gmail.com>
2023-02-25 13:41:05 -0500

    update local url

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const multer = require("multer");
-const upload = multer({dest: "uploads/"});
+const upload = multer({ dest: "uploads/" });
 const catImageRoutes = require("./routes/catImageRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
 const CatImage = require("./models/CatImage.js");
@@ -12,7 +12,6 @@ const User = require("./models/User.js");
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -24,7 +23,7 @@ app.use("/auth", authRoutes);
 // Connect to MongoDB database
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect("mongodb://localhost/meowmadness", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -36,13 +35,15 @@ mongoose
 
 app.post("/upload", upload.single("image"), async (req, res) => {
   try {
-    const {filename} = req.file;
-    const user = await User.findOne({username: req.body.username});
+    const { filename } = req.file;
+
+    const user = await User.findOne({ username: req.body.username });
     const image = new CatImage({
       filename,
       user: user._id,
-      imageUrl: "/uploads/" + filename,
+      imageUrl: "http://localhost:5000/uploads/" + filename,
     });
+
     await image.save();
     res.json(image);
   } catch (err) {
