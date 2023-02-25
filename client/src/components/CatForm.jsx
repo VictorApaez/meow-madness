60b8d4f77bf1ae543001aad673133f99e14d8f4a
VictorApaez <vic.apaez@gmail.com>
2023-02-25 13:36:41 -0500

    sending image to db and getting img as response

--- client/src/components/CatForm.jsx
+++ client/src/components/CatForm.jsx
@@ -1,9 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../styles/CatForm.css";
 import Modal from "react-modal";
+import { uploadCatImg } from "../services/uploadService";
 
 function CatForm() {
   const [toggleCatForm, setToggleCatForm] = useState(false);
+  const [file, setFile] = useState(null);
+  const [imageData, setImageData] = useState(null);
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("username", "victor");
+    const res = await uploadCatImg(formData);
+    setImageData(res.data);
+    setToggleCatForm(false);
+  }
+
   return (
     <div className="cat-form">
       <button
@@ -12,6 +26,7 @@ function CatForm() {
       >
         Upload
       </button>
+      {imageData && <img src={imageData.imageUrl}></img>}
       <Modal
         isOpen={toggleCatForm}
         onRequestClose={() => setToggleCatForm(false)}
@@ -22,7 +37,16 @@ function CatForm() {
         >
           X
         </button>
-        <h2 className="cat-form__title">Upload your cat pic</h2>
+        <form onSubmit={(e) => handleFormSubmit(e)}>
+          <h2 className="cat-form__title">Upload your cat pic</h2>
+          <input
+            type="file"
+            id="file-input"
+            name="image"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <input type="submit" />
+        </form>
       </Modal>
     </div>
   );
