d932e2c407d3bbd426894f0a429f2da51c3e8d72
Brandon Alvarez <balvarez325@gmail.com>
2023-02-25 15:43:22 -0500

    update upload aside bar css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -32,6 +32,19 @@ function Home() {
       <aside className="upload-div">
         <div className="upload-btn">Upload</div>
         <div className="upload-btn">Vote</div>
+        <div className="rules">
+          <h3>Rules</h3>
+          <ul>
+            <li>Rule 1</li>
+            <li>Rule 2</li>
+            <li>Rule 3</li>
+          </ul>
+          <ul>
+            <li>Rule 4</li>
+            <li>Rule 5</li>
+            <li>Rule 6</li>
+          </ul>
+        </div>
       </aside>
     </div>
   );
