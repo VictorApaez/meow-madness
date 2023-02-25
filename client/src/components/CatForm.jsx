d96fe064933a12b6b36c2ca5af62f37b5da3a1c9
VictorApaez <vic.apaez@gmail.com>
2023-02-25 09:42:49 -0500

    added modal

--- client/src/components/CatForm.jsx
+++ client/src/components/CatForm.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 function CatForm() {
-  return <div>CatForm</div>;
+  const [toggleCatForm, setToggleCatForm] = useState(false);
+  return (
+    <div className="cat-form">
+      <button
+        className="cat-form__toggle-btn"
+        onClick={() => setToggleCatForm(true)}
+      >
+        Upload
+      </button>
+      <Modal
+        isOpen={toggleCatForm}
+        onRequestClose={() => setToggleCatForm(false)}
+      >
+        <button
+          className="cat-form__toggle-btn"
+          onClick={() => setToggleCatForm(false)}
+        >
+          X
+        </button>
+        <h2 className="cat-form__title">Upload your cat pic</h2>
+      </Modal>
+    </div>
+  );
 }
 
 export default CatForm;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import Modal from "react-modal";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+Modal.setAppElement(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
