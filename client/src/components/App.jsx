dcf074240f7f2e662f512ddf89c687f8c6907d08
VictorApaez <vic.apaez@gmail.com>
2023-02-25 08:24:12 -0500

    created a route for home, login, signup, and catform

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,20 @@
 import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./Signup";
+import Login from "./Login";
+import Home from "./Home";
+import CatForm from "./CatForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/catform" element={<CatForm />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
