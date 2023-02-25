2b38d93f2cb3ad19e8cc6949f6bcb702b3b4eafb
VictorApaez <vic.apaez@gmail.com>
2023-02-25 16:04:33 -0500

    created signup input validation

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Signup.css";
 import { Link } from "react-router-dom";
+import { signup } from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
+  console.log("hit");
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const validationErrors = {};
+    if (!formData.username) {
+      validationErrors.username = "Username is required";
+    }
+    if (!formData.password) {
+      validationErrors.password = "Password is required";
+    }
+    const res = await signup(formData);
+    if (!res.success) {
+      validationErrors.other = res.error;
+    } else {
+      localStorage.setItem("token", res.token);
+      navigate("/home");
+    }
+    console.log(validationErrors);
+    //TODO: set validation error
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      setErrors({});
+    }
+  }
+
   return (
     <div className="signup">
       <h2 className="signup__title">Sign Up</h2>
-      <form className="signup__form">
+      <form className="signup__form" onSubmit={(e) => handleSubmit(e)}>
         <div className="signup__form-group">
           <label htmlFor="username" className="signup__label">
             Username
@@ -16,9 +53,12 @@ function Signup() {
             id="username"
             name="username"
             className="signup__input"
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
           />
         </div>
-
+        {errors.username && <p className="signup__error">{errors.username}</p>}
         <div className="signup__form-group">
           <label htmlFor="password" className="signup__label">
             Password
@@ -28,11 +68,18 @@ function Signup() {
             id="password"
             name="password"
             className="signup__input"
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
           />
         </div>
+        {errors.password && <p className="signup__error">{errors.password}</p>}
+
         <button type="submit" className="signup__button">
           Sign Up
         </button>
+        {errors.other && <p className="signup__error">{errors.other}</p>}
+
         <p className="signup__login-link">
           Already have an account?{" "}
           <Link to="/login" className="signup__link">
