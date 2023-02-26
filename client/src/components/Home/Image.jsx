01b0685c9d50910d910c7f19139ea564a5add1fd
Brandon Alvarez <balvarez325@gmail.com>
2023-02-25 23:38:01 -0500

    update styles

--- client/src/components/Home/Image.jsx
+++ client/src/components/Home/Image.jsx
@@ -4,8 +4,8 @@ import "./Image.css";
 function Image() {
   return (
     <div className="image">
-      <div className="image-vote-count"></div>
-      <div className="image-description"></div>
+      <div className="image-vote-count">100 Votes</div>
+      <div className="image-description">Cat by Username</div>
     </div>
   );
 }
