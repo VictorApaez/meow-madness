3c9a73b9eeb23fbd649bfac3b6bb4ce215b8336c
VictorApaez <vic.apaez@gmail.com>
2023-02-25 19:39:49 -0500

    home changes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,7 +2,6 @@ import { TopPicks } from "./TopPicks";
 import { Pof } from "./Pof";
 import { Aside } from "./Aside";
 import React from "react";
-import Image from "./Image";
 import "./Home.css";
 
 function Home() {
