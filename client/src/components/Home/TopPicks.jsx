9ceb1b78e8c36f3e619ac31328b96257a206ef49
Brandon Alvarez <balvarez325@gmail.com>
2023-02-25 19:52:10 -0500

    create ranking blocks

--- client/src/components/Home/TopPicks.jsx
+++ client/src/components/Home/TopPicks.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Image from "./Image";
+import "./TopPicks.css";
 
 export function TopPicks({}) {
   return (
     <div className="current-rankings">
       <h2>Current Rankings</h2>
       <div className="top-div">
+        <div class="rank">1</div>
         <Image />
+        <div class="rank">2</div>
         <Image />
+        <div class="rank">3</div>
         <Image />
       </div>
     </div>
