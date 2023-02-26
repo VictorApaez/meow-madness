45e5569be027fd4881a12530d990c2cdd45dd1ff
Brandon Alvarez <balvarez325@gmail.com>
2023-02-25 20:53:18 -0500

    update home layout

--- client/src/components/Home/TopPicks.jsx
+++ client/src/components/Home/TopPicks.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "./Image";
 import "./TopPicks.css";
+import crown from "../../assets/images/Ranking_1st_Place.png";
 
-export function TopPicks({}) {
+export function TopPicks() {
   return (
     <div className="current-rankings">
       <h2>Current Rankings</h2>
+      <img class="first-place" src={crown} alt="crown" width="100" />
       <div className="top-div">
         <div class="rank">1</div>
         <Image />
